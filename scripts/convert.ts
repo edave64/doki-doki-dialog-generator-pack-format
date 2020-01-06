6d3cea982e9ed04f518ed4295f51be9cbd215cf8
EDave64 <nord3rn@gmail.com>
2020-01-06 23:25:22 +0100

    Added v1 conversion script

---
+++ scripts/convert.ts
@@ -0,0 +1,35 @@
+import { normalizeCharacter } from '../src/v1/parser';
+import { convert } from '../src/v2/convertV1';
+import { assetWalker } from '../src/v2/util';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+let input = '';
+
+process.stdin.on('data', function(chunk) {
+	input += chunk;
+});
+
+process.stdin.on('end', function() {
+	convertJSON();
+});
+
+function convertJSON() {
+	const v1pack = normalizeCharacter(JSON.parse(input), { '/': '/' });
+	if (!v1pack.packId) v1pack.packId = 'fisch';
+	const v2pack = convert(v1pack, true);
+	let chibi = true;
+
+	const extensions = assetWalker(v2pack, (old, type) => {
+		if (type === 'font') throw new Error(`Fonts weren't supported in V1. WTH?`);
+		old = old + '{ext}';
+		if (chibi) {
+			chibi = false;
+			return old;
+		} else {
+			return `/${old}`;
+		}
+	});
+	console.log(JSON.stringify(extensions, undefined, 2));
+}
